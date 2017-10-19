cd1120fa4036b6bf8aadebbefd7e841686c8e86c
rpazda <richard.pazda@gmail.com>
2017-10-19 09:43:15 -0400

    Added some code for models, Added main game code

--- Assignment 4 - Board Game/js/arbitrarium.js
+++ Assignment 4 - Board Game/js/arbitrarium.js
@@ -1,12 +1,27 @@
 	
 	/*
-	
+		Create a board game. 
+		It must load in object models. 
+		It must use raycasting to detect what piece the mouse clicked on.
+
+		sound effects 
+		music.
 	*/
 
 	/*
 		Ideas:
-			
+			Arbitrarium
 
+				Game of chance with known and unknown probability elements
+				Square board with movement based on "dice roll"
+				Point deductions for not completing turn in time
+				Mystery squares
+				
+	*/
+
+	/* 
+		TODO:
+			Game ended condition
 	*/
 	
 	var renderer;
@@ -14,9 +29,18 @@
 	var camera;
 	var spotlight;
 
+	var mouse = new THREE.Vector2();
+	var selectedobject = null;
+	
+	var raycaster = new THREE.Raycaster();
+	var projector = new THREE.Projector();
+
 	var defaultMusic;
 
 	var musicPlaying = true;
+	var soundEnabled = true;
+	var gamePaused = false;
+	var gameEnded = false;
 	
 	Physijs.scripts.worker = 'libs/physijs_worker.js';
 	Physijs.scripts.ammo = 'ammo.js';
@@ -34,7 +58,7 @@
 	{
 		//scene = new THREE.Scene();
 		scene = new Physijs.Scene();
-		scene.setGravity(new THREE.Vector3(0, 0, gravityFactor));
+		//scene.setGravity(new THREE.Vector3(0, 0, gravityFactor));
 
 		camera = new THREE.PerspectiveCamera(
 		// frustum vertical view         aspect ratio							 frustum near plane     frustum far plane
@@ -45,11 +69,13 @@
 		setupSpotlight();
 		
 		// Main code here.
-		initGamePanel();
-		loadTextures();
-		loadMusic();
-		loadSounds();
-		createFloatingText();
+		//initGamePanel();
+		//loadTextures();
+		//loadMusic();
+		//loadSounds();
+		//createFloatingText();
+
+		loadZune();
 		
 		// Output to the stream
 		//document.body.appendChild( renderer.domElement );
@@ -72,9 +98,9 @@
 	
 	function setupCamera()
 	{
-		camera.position.x = 0;
-		camera.position.y = 0;
-		camera.position.z = 0;
+		camera.position.x = 100;
+		camera.position.y = 100;
+		camera.position.z = 100;
 		camera.lookAt( scene.position );
 	}
 	
@@ -137,13 +163,16 @@
 	//Render function to update scene
 	function render()
 	{	
-		scene.simulate();
+		if(gamePaused == false && !gameEnded)
+        {
+			scene.simulate();
 
-		// Request animation frame
-		requestAnimationFrame( render );
-		
-		// Call render()
-		renderer.render( scene, camera );
+			// Request animation frame
+			requestAnimationFrame( render );
+			
+			// Call render()
+			renderer.render( scene, camera );
+		}
 	}
 
 	function loadTextures()
@@ -170,6 +199,50 @@
 		
 	}
 
+	///Load the game board
+	function loadGameBoard()
+	{
+
+	}
+
+	///Load player pieces
+	function loadPlayerPieces()
+	{
+
+	}
+
+	function loadZune( )
+	{
+		// instantiate a loader
+		var loader = new THREE.OBJMTLLoader();
+		
+		// load an obj / mtl resource pair
+		loader.load(
+			// OBJ resource URL
+			'assets/zune_120_obj/zune_120.obj',
+
+			// MTL resource URL	
+			'assets/zune_120_obj/rook.mtl',
+			
+			// Function when both resources are loaded			// Function when both resources are loaded
+			function ( object ) 
+			{
+				// Added to fix raycasting
+				object.castShadow = true;
+				object.receiveShadow = true;
+				object.scale.set( .3, .3, .3 );
+				
+				var obj = new THREE.Object3D();
+				obj.name = 'Zune';
+				object.parent = obj;
+				obj.add( object );
+
+				scene.add(obj);
+
+				//obj.rotation.x = -(Math.PI/2);
+			}
+		);
+	}
 
 	///Turn music on/off
 	function toggleMusic(){
@@ -186,6 +259,35 @@
 			musicPlaying = false;
 		}
 	}
+
+	//Enable/disable sounds
+	function toggleSounds(){
+		if(soundEnabled == false)
+		{
+			$('#sounds-toggle').html("Sounds Off");
+			soundEnabled = true;
+		}
+		else
+		{	
+			$('#sounds-toggle').html("Sounds On");
+			soundEnabled = false;
+		}
+	}  
+
+	///Toggle pause/unpause
+    function pauseGame()
+    {
+        if(gamePaused == false)
+		{
+			$('#pause-button').html("Unpause Game");
+			gamePaused = true;
+		}
+		else
+		{	
+			$('#pause-button').html("Pause Game");
+			gamePaused = false;
+		}
+    }
 
 	///Add non-funtional game details
 	function addProps()
@@ -223,4 +325,3 @@
 	}
 	
 	window.onload = init();
-
